a71688ae06b85426e16ce36af66557a18040eaf2
Bimogaty <happybirthday1157@gmail.com>
2024-11-25 23:23:57 +0700

    change complieSdk & targetSdk from 34 to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "kh.edu.rupp.ite.mobileapp"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "kh.edu.rupp.ite.mobileapp"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 

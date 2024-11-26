3f271c6695bb8a6494f665507b408fd0b6b5ce68
Natsu911 <measpanhaseth@gmail.com>
2024-11-26 15:12:03 +0700

    Add Bottom NavBar UI

---
+++ app/src/main/java/kh/edu/rupp/ite/mobileapp/MainActivity.kt
@@ -0,0 +1,24 @@
+package kh.edu.rupp.ite.ebooking
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Get NavController
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController: NavController = navHostFragment.navController
+
+        // Setup BottomNavigationView with NavController
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNavigationView.setupWithNavController(navController)
+    }
+}

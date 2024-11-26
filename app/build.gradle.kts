5989d2837506b29600ba9c0cdf58659c8637c21c
Natsu911 <measpanhaseth@gmail.com>
2024-11-26 15:11:50 +0700

    Add Bottom NavBar UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,13 +1,14 @@
 plugins {
     id("com.android.application")
+    id("org.jetbrains.kotlin.android")
 }
 
 android {
-    namespace = "kh.edu.rupp.ite.mobileapp"
+    namespace = "kh.edu.rupp.ite.ebooking"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "kh.edu.rupp.ite.mobileapp"
+        applicationId = "kh.edu.rupp.ite.ebooking"
         minSdk = 24
         targetSdk = 35
         versionCode = 1
@@ -29,14 +30,23 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
 }
 
 dependencies {
 
+    implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+
+    //Navigation
+    implementation ("androidx.navigation:navigation-fragment-ktx:2.7.3")
+    implementation ("androidx.navigation:navigation-ui-ktx:2.7.3")
+
 }
